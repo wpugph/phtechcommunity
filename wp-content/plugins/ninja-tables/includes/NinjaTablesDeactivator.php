d8a7d6b44468358aac88ad21c447e0a7a6e81a84
Carl Alberto <carl.alberto@getpantheon.com>
2018-08-14 19:13:18 +0000

    installed ninja tables

---
+++ wp-content/plugins/ninja-tables/includes/NinjaTablesDeactivator.php
@@ -0,0 +1,36 @@
+<?php namespace NinjaTables\Classes;
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://authlab.io
+ * @since      1.0.0
+ *
+ * @package    Wp_table_data_press
+ * @subpackage Wp_table_data_press/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_table_data_press
+ * @subpackage Wp_table_data_press/includes
+ * @author     Shahjahan Jewel <cep.jewel@gmail.com>
+ */
+class NinjaTablesDeActivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
