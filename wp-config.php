1420bb981a16da8b5ee5d40af4862ab683d8d4be
Carl Alberto <carl.alberto@getpantheon.com>
2019-07-16 16:48:37 +0800

    custom redirect code setuped

--- wp-config.php
+++ wp-config.php
@@ -147,7 +147,7 @@ if ( ! defined( 'WP_DEBUG' ) ) {
     define('WP_DEBUG', false);
 }
 
-if ( file_exists( dirname( __FILE__ ) . '/wp-config-custom.php' ) && !isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ) {
+if ( file_exists( dirname( __FILE__ ) . '/wp-config-custom.php' ) && isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ) {
   require_once(dirname(__FILE__) . '/wp-config-custom.php');
 }
 
